7ddcd03c3cf451c0cd69737ffa98355569f0b768
Oliver Uvman <oliver.uvman@gmail.com>
2012-06-18 02:22:44 +0200

    Compact emoji listing

--- emojis.pl
+++ emojis.pl
@@ -1,5 +1,6 @@
 use strict;
 use vars qw($VERSION %IRSSI);
+use List::Util qw(max)
 
 use Irssi;
 
@@ -102,6 +103,60 @@ sub emojitable {
     Irssi::printformat(MSGLEVEL_CLIENTCRAP, 'tblf', "End emojis");
 }
 
+sub ensureLength {
+    ($length, $str) = @_;
+    $result = $str;
+    while(length($result) < $length) {
+	$result .= " ";
+    }
+}
+
+sub emojiprettyprint {
+    my $leftmargin = 12;  # TODO should depend on timestamp format etc
+    my $width = $window->{'width'} - $leftmargin;  # The room we have to print in
+    my $spacing = 2;
+    my @emojis = ();
+    my @triggers = ();
+
+    #  Insert tuples into rows with greedy algo
+    while ( my($trigger, $emoji) = each(%EMOJIS) ) {
+	my $tupleLength = max($trigger, $emoji) + $spacing;
+	my $adjustedTrigger = ensureLength($tupleLength, $trigger);
+	my $adjustedEmoji = ensureLength($tupleLength, $emoji);
+
+	if ($@emojis == 0) {
+	    #  First tuple, add to new line
+	    push(@emojis, $adjustedEmoji);
+	    push(@triggers, $adjustedTrigger);
+	} else {
+	    my $didPut = 0;
+	    for ($i=0; $i < $#emojis; $i++) {
+		#  Add tuple to longest possible line
+		if (length(@emojis[$i]+$tupleLength <= $width)) {
+		    @emojis[$i] .= $adjustedEmoji;
+		    @triggers[$i] .= $adjustedTrigger;
+		    $didPut = 1;
+		}
+	    }
+	    if ($didPut == 0) {
+		#  If no line could fit tuple, put in new line
+		push(@emojis, $adjustedEmoji);
+		push(@triggers, $adjustedTrigger);
+	    }
+
+	    #  Sort lines with longest first
+	    @emojis = sort {length $b <=> length $a} @emojis;
+	    @triggers = sort {length $b <=> length $a} @triggers;
+	}
+    }
+    Irssi::printformat(MSGLEVEL_CLIENTCRAP, 'tblh', "List of emojis");
+    for ($i=0; $i < $#emojis; $i++) {
+	Irssi::printformat(MSGLEVEL_CLIENTCRAP, 'tbl', @trigger[$i]);
+	Irssi::printformat(MSGLEVEL_CLIENTCRAP, 'tbl', @emoji[$i]);
+    }
+    Irssi::printformat(MSGLEVEL_CLIENTCRAP, 'tblf', "End emojis");
+}
+
 # Settings
 Irssi::settings_add_bool('lookandfeel', 'knifamode_enable', 1);
 Irssi::settings_add_str('lookandfeel', 'knifamode_dbfile',
@@ -112,6 +167,7 @@ Irssi::signal_add_first('send command', 'knifaize');
 
 # commands
 Irssi::command_bind emojis => \&emojitable;
+Irssi::command_bind eee => \&emojiprettyprint;
 Irssi::command_bind reloademojis => \&reload_emojis;
 
 # Register formats for table-like display.
